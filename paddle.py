8f5bac9748562392908d88958974d69728336832
Galina <galinarudnitca@gmail.com>
2024-04-03 10:14:54 +0100

    Created a new Paddle class

---
+++ paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+class Paddle(Turtle): # Paddle class inherits all the functionality of Turtle class
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5, 1)
+        self.penup()
+        self.goto(position)
+        
+        
+    def go_up(self):
+        
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y) 
+
+
+
--- pong_main.py
+++ pong_main.py
@@ -1,31 +1,22 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
-paddle = Turtle()
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.setup(width=800, height=600)
 screen.bgcolor("grey")
 screen.title("Pong Game")
 screen.tracer(0)
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
-
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(5, 1)
-paddle.penup()
-paddle.goto(x=350, y=0)
 
 
 
