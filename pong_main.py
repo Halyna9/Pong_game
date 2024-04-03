dc6d3ce9f3554428718dc5d87db1f6a6d7a8fce9
Galina <galinarudnitca@gmail.com>
2024-04-03 11:55:38 +0100

    Detect collisuons

--- pong_main.py
+++ pong_main.py
@@ -1,9 +1,13 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.setup(width=800, height=600)
 screen.bgcolor("grey")
@@ -18,13 +22,19 @@ screen.onkey(l_paddle.go_down, "s")
 
 
 
-
-
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
 
